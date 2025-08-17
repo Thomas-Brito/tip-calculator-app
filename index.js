da02bbdedf2d35518286c1f46420bccc437d5e9f
thomasbrito <thomasbrito.tjb@gmail.com>
2025-08-17 19:25:09 -0300

    Refinamentos

--- index.js
+++ index.js
@@ -1,90 +1,123 @@
 const
-  id = (id) => document.getElementById(id),
-  bill = id(`app__bill-input`),
-  options = id(`app-d-tip-percent-values`),
-  numberPeople = id(`app__number-people-input`),
-  resetButton = id(`app__reset-button`),
-  numberInputs = document.querySelectorAll(`input[type=number]`),
-  tipPerson = id(`tip-amount-person`),
-  totalPerson = id(`total-person`),
-  customPercent = id(`app__custom-percent`),
-  numberPeopleLabelContainer = id(`app-d-number-people-labels`),
-  numberPeopleLabel = id(`app__text-label--error`),
-  percentOptions = Array.from(document.getElementsByClassName(`app__percent`)); 
+  id = (elementId) => document.getElementById(elementId),
+  elBillInput = id(`app__bill-input`),
+  elCustomPercent = id(`app__custom-percent`),
+  elNumberPeopleInput = id(`app__number-people-input`),
+  elNumberPeopleLabelContainer = id(`app-d-number-people-labels`),
+  elNumberPeopleLabel = id(`app__text-label--error`),
+  elOptionsContainer = id(`app-d-tip-percent-values`),
+  elResetButton = id(`app__reset-button`),
+  elTipPerson = id(`tip-amount-person`),
+  elTotalPerson = id(`total-person`),
 
-const tipCalculator = () => {
+  elsNumberInputs = document.querySelectorAll(`input[type=number]`),
+  elsPercentOptions = Array.from(document.getElementsByClassName(`app__percent`)),
 
-  const
-    getValidPeopleCount = people => people >= 1 ? people : 1,
-    percentMap = {
-      'five': 5,
-      'ten': 10,
-      'fifteen': 15,
-      'twenty-five': 25,
-      'fifty': 50
+  getFormValues = () => {
+    return {
+      valBillInput: Number(elBillInput.value),
+      valCustomPercent: Number(elCustomPercent.value),
+      valNumberPersonInput: Number(elNumberPeopleInput.value),
+      elPercentOption: elsPercentOptions.find(option => option.checked)
     }
+  },
 
-  let
-    percentCurrentOption = percentOptions.find(option => option.checked),
-    customPercentValue = Number(customPercent.value),
-    percent = percentMap[percentCurrentOption?.value] ?? customPercentValue,
-    billValue = Number(bill.value),
-    tipTotal = (billValue * percent / Number(100)),
-    numberPeopleValue = Number(numberPeople.value),
-    tipPerPerson = tipTotal / getValidPeopleCount(numberPeopleValue),
-    totalPerPerson = (billValue + tipTotal) / getValidPeopleCount(numberPeopleValue);
+  calculateTip = (formValues) => {
+    const
+      {valBillInput, valCustomPercent, valNumberPersonInput, elPercentOption} = formValues,
+      getValidNumberPerson = people => people >= 1 ? people : 1,
+      mapPercentValues = {
+        'five': 5,
+        'ten': 10,
+        'fifteen': 15,
+        'twenty-five': 25,
+        'fifty': 50
+      },
+      valPercent = mapPercentValues[elPercentOption?.value] ?? valCustomPercent,
+      valTip = valBillInput * valPercent / Number(100),
+      valNumberPerson = Math.trunc(getValidNumberPerson(valNumberPersonInput)),
+      valTipPerson = valTip / valNumberPerson,
+      valTotalPerson = (valBillInput + valTip) / valNumberPerson;
 
-  return {tipPerPerson, totalPerPerson}
-}
+    return {valTipPerson, valTotalPerson}
+  },
 
-const updateUI = (results) => {
+  updateUI = (results) => {
+    const {valTipPerson, valTotalPerson} = results;
 
-  let {tipPerPerson, totalPerPerson} = results;
+    elTipPerson.innerText = `$${valTipPerson.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+    elTotalPerson.innerText = `$${valTotalPerson.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+  },
 
-  tipPerson.innerText = `$${tipPerPerson.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-  totalPerson.innerText = `$${totalPerPerson.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-}
+  handleDOM = () => {
+    updateUI(calculateTip(getFormValues()));
+  },
 
-const updateCustomPercentUI = () => {
-
-  if (document.activeElement === customPercent) {
-    percentOptions.forEach(option => option.checked = false);
-    customPercent.classList.add(`app__button-label-v-custom--active`);
-  } else if (percentOptions.some(option => option.checked === true)) {
-      customPercent.classList.remove(`app__button-label-v-custom--active`);
-    }
-}
-
-const validateNumberPeople = (peopleCount) => {
-
-  if (document.activeElement === numberPeople) {
-    if (peopleCount <= 0) {
-      numberPeopleLabelContainer.classList.add(`app-d-number-people-labels--error`);
-      numberPeople.classList.add(`app__text-input--error`);
-      numberPeopleLabel.innerText = `Enter a number greater than zero`;
-    } else {
-        numberPeopleLabelContainer.classList.remove(`app-d-number-people-labels--error`);
-        numberPeople.classList.remove(`app__text-input--error`);
-        numberPeopleLabel.innerText = ``;
+  updateCustomPercentUI = () => {
+    if (document.activeElement === elCustomPercent) {
+      elsPercentOptions.forEach(option => option.checked = false);
+      elCustomPercent.classList.add(`app__button-label-v-custom--active`);
+    } else if (elsPercentOptions.some(option => option.checked === true)) {
+        elCustomPercent.classList.remove(`app__button-label-v-custom--active`);
       }
-  }
-}
+  },
 
-const resetValues = () => {
-  bill.value = ``;
-  percentOptions.forEach(option => option.checked = false);
-  numberPeople.value = Number(1);
-  tipPerson.innerText = `$0.00`;
-  totalPerson.innerText = `$0.00`;
-  customPercent.value = ``;
-  customPercent.classList.remove(`app__button-label-v-custom--active`);
-  numberPeopleLabelContainer.classList.remove(`app-d-number-people-labels--error`);
-  numberPeople.classList.remove(`app__text-input--error`);
-  numberPeopleLabel.innerText = ``;
-}
+  validateNumberPeople = (valNumberPeople) => {
+    if (valNumberPeople <= 0) {
+      return {
+        isValid: false,
+        message: `Invalid value. Division will be done by 1.`
+      }
+    } else if (!Number.isInteger(valNumberPeople)) {
+        return {
+          isValid: false,
+          message: `Invalid value. Division will be done by ${Math.trunc(valNumberPeople)}.`
+        }
+      } else {
+          return {
+            isValid: true,
+            message: ``
+          }
+        }
+  },
 
-bill.addEventListener(`input`, () => {updateUI(tipCalculator())});
-options.addEventListener(`input`, () => {updateUI(tipCalculator()); updateCustomPercentUI()});
-numberPeople.addEventListener(`input`, () => {updateUI(tipCalculator()); validateNumberPeople(Number(numberPeople.value))});
-resetButton.addEventListener(`click`, () => {resetValues()});
-numberInputs.forEach(option => option.addEventListener(`focus`, option.select));
+  applyValidationNumberPeopleUI = (valValidationNumberPeople) => {
+    const {isValid, message} = valValidationNumberPeople;
+
+    if (!isValid) {
+      elNumberPeopleLabelContainer.classList.add(`app-d-number-people-labels--error`);
+      elNumberPeopleInput.classList.add(`app__text-input--error`);
+      elNumberPeopleLabel.innerText = message;
+    } else {
+      elNumberPeopleLabelContainer.classList.remove(`app-d-number-people-labels--error`);
+      elNumberPeopleInput.classList.remove(`app__text-input--error`);
+      elNumberPeopleLabel.innerText = message;
+    }
+  },
+
+  resetValuesUI = () => {
+    elBillInput.value = ``;
+
+    elCustomPercent.value = ``;
+    elCustomPercent.classList.remove(`app__button-label-v-custom--active`);
+
+    elNumberPeopleInput.classList.remove(`app__text-input--error`);
+    elNumberPeopleInput.value = ``;
+    elNumberPeopleLabel.innerText = ``;
+    elNumberPeopleLabelContainer.classList.remove(`app-d-number-people-labels--error`);
+
+    elTipPerson.innerText = `$0.00`;
+    elTotalPerson.innerText = `$0.00`;
+
+    elsPercentOptions.forEach(option => option.checked = false);
+  };
+
+elBillInput.addEventListener(`input`, () => {handleDOM()});
+elOptionsContainer.addEventListener(`input`, () => {handleDOM();
+  updateCustomPercentUI();
+});
+elNumberPeopleInput.addEventListener(`input`, (event) => {handleDOM();
+  applyValidationNumberPeopleUI(validateNumberPeople(Number(event.target.value)));  
+});
+elResetButton.addEventListener(`click`, () => {resetValuesUI()});
+elsNumberInputs.forEach(option => option.addEventListener(`focus`, option.select));
